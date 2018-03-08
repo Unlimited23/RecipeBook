70db4c089e2311ecdb3d61f0c717aaec11220e29
Unlimited23 <petyoruzhin@gmail.com>
2018-03-08 04:40:39 +0200

    fix: dummy recipe detail + typo fix

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Mandjata na Pesho', 'Pesho test recipe', 'https://instagram.fsof3-1.fna.fbcdn.net/vp/d6ed657e19902b00336d573f2d0f8aa0/5B28F2A8/t51.2885-15/e35/13266708_1291043794242404_1140537066_n.jpg');
+    new Recipe('Mandjata na Pesho', 'Pesho test recipe', 'https://instagram.fsof3-1.fna.fbcdn.net/vp/d6ed657e19902b00336d573f2d0f8aa0/5B28F2A8/t51.2885-15/e35/13266708_1291043794242404_1140537066_n.jpg'),
   ];
 
   constructor() { }
