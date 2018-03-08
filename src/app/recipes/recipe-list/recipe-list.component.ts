4e10732c4319dee21f18740202a0e027e6b37365
Unlimited23 <petyoruzhin@gmail.com>
2018-03-08 04:25:59 +0200

    fix: added a dummy recipe item in the recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Mandjata na Pesho', 'Pesho test recipe', 'https://instagram.fsof3-1.fna.fbcdn.net/vp/d6ed657e19902b00336d573f2d0f8aa0/5B28F2A8/t51.2885-15/e35/13266708_1291043794242404_1140537066_n.jpg');
+  ];
 
   constructor() { }
 
