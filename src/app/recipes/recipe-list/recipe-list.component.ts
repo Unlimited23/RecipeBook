5485e7f22c63e1c26bd469bf76dba5c4e02ab8b6
Unlimited23 <petyoruzhin@gmail.com>
2018-03-09 05:03:22 +0200

    add: recipe detail functionality

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,8 +7,10 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Mandjata na Pesho', 'Pesho test recipe', 'https://instagram.fsof3-1.fna.fbcdn.net/vp/d6ed657e19902b00336d573f2d0f8aa0/5B28F2A8/t51.2885-15/e35/13266708_1291043794242404_1140537066_n.jpg'),
+    new Recipe('Drugata mandja na Pesho', 'Pesho test recipe 2', 'https://instagram.fsof3-1.fna.fbcdn.net/vp/d6ed657e19902b00336d573f2d0f8aa0/5B28F2A8/t51.2885-15/e35/13266708_1291043794242404_1140537066_n.jpg'),
   ];
 
   constructor() { }
@@ -16,4 +18,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
